300c3c60a2e9d00ee8a3b15fcf3f3df94595a9f5
Donnadeesha <52916610+Donnadeesha@users.noreply.github.com>
2021-02-06 03:33:49 +0530

    class link

--- app/src/main/java/com/example/suntech_application/MainActivity.java
+++ app/src/main/java/com/example/suntech_application/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.suntech_application;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,10 +19,28 @@ public class MainActivity extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
         getSupportActionBar().hide();
-
-
-
         setContentView(R.layout.activity_main);
 
+
+
+
+        Button btnRegNow = (Button)findViewById(R.id.btnRegister);
+
+
+        btnRegNow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openRegisterMainUI();
+            }
+        });
+
+
     }
+
+    private void openRegisterMainUI() {
+        Intent intent = new Intent(MainActivity.this,RegisterMainUI.class);
+        startActivity(intent);
+    }
+
+
 }
