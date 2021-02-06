b60dd5dea79f6dca35bb6b75e17e66773ba1caf7
Donnadeesha <52916610+Donnadeesha@users.noreply.github.com>
2021-02-06 23:40:20 +0530

    Model class

--- app/src/main/java/com/example/suntech_application/Admin/Admin.java
+++ app/src/main/java/com/example/suntech_application/Admin/Admin.java
@@ -1,4 +1,50 @@
 package com.example.suntech_application.Admin;
 
 public class Admin {
+
+
+    private String sid;
+    private String Name;
+    private String userName;
+    private String password;
+
+
+    public Admin(){
+
+    }
+
+    public String getSid() {
+        return sid;
+    }
+
+    public void setSid(String sid) {
+        this.sid = sid;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+
 }
--- app/src/main/java/com/example/suntech_application/Laptop/Laptop.java
+++ app/src/main/java/com/example/suntech_application/Laptop/Laptop.java
@@ -1,4 +1,62 @@
 package com.example.suntech_application.Laptop;
 
 public class Laptop {
+
+
+    private String id;
+    private String model;
+    private String quantity;
+    private String username;
+    private String password;
+
+
+
+    public Laptop(){
+
+    }
+
+    public String getId(){
+        return id;
+    }
+
+    public void setId(String id){
+        this.id=id;
+    }
+
+    public String getModel(){
+        return model;
+    }
+
+    public void setModel(String model){
+        this.model=model;
+    }
+
+    public String getQuantity(){
+        return quantity;
+    }
+
+    public void setQuantity(String quantity){
+        this.quantity=quantity;
+    }
+
+
+    public String getUserName() {
+        return username;
+    }
+
+    public void setUserName(String userName) {
+        this.username = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+
+
 }
